13cfe8414937835d18f9ef3a45dd47fadee6b693
mailinh221100 <mailinhtvtcna@gmail.com>
2022-07-22 09:36:48 +0700

    add module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WebShellModule } from '@shell/web-shell.module';
+import { NgToastModule } from 'ng-angular-popup';
 import { CoreModule } from './@core/core.module';
 import { AppComponent } from './app.component';
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, CoreModule, WebShellModule, BrowserAnimationsModule],
+    imports: [BrowserModule, CoreModule, WebShellModule, BrowserAnimationsModule,ReactiveFormsModule, NgToastModule],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
