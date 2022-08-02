e0b18087a93d94323119990a0136aa12aabe64f6
mquan247 <quanngo247@gmail.com>
2022-08-02 09:40:15 +0700

    add filter

--- src/app/pages/category/category.module.ts
+++ src/app/pages/category/category.module.ts
@@ -1,23 +1,24 @@
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { MatButtonModule } from "@angular/material/button";
-import { MatPaginatorModule } from "@angular/material/paginator";
-import { MatTableModule } from "@angular/material/table";
-import { CategoryRoutingModule } from "./category-routing.module";
-import { CityPage } from "./pages/city/city.page";
-
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { CategoryRoutingModule } from './category-routing.module';
+import { CityPage } from './pages/city/city.page';
 @NgModule({
-    declarations: [
-        CityPage
-    ],
+    declarations: [CityPage],
     imports: [
         FormsModule,
         CommonModule,
         CategoryRoutingModule,
         MatButtonModule,
         MatTableModule,
-        MatPaginatorModule
+        MatPaginatorModule,
+        MatFormFieldModule,
+        MatInputModule,
     ],
-  })
+})
 export class CategoryModule {}
--- src/app/pages/category/pages/city/city.page.ts
+++ src/app/pages/category/pages/city/city.page.ts
@@ -6,20 +6,19 @@ import { FormDialogComponent } from '@components/form-dialog/form-dialog.compone
 import { City } from '@core/models/city.model';
 import { CityService } from '@core/services/basic/city.service';
 import { NgToastService } from 'ng-angular-popup';
-
 @Component({
     templateUrl: './city.page.html',
     styleUrls: ['./city.page.css'],
 })
 export class CityPage implements OnInit, AfterViewInit {
-    abbName= '';
-    name= '';
-    selectedId= '';
+    abbName = '';
+    name = '';
+    selectedId = '';
     currentPage = 0;
-    pageSize = 10;
+    pageSize = 5;
 
     dataSource = new MatTableDataSource<City>();
-    total= 0;
+    total = 0;
     displayedColumns: string[] = ['name', 'abbName', 'action'];
 
     constructor(
@@ -32,8 +31,8 @@ export class CityPage implements OnInit, AfterViewInit {
         this.getList();
     }
 
-    ngAfterViewInit(){
-        return
+    ngAfterViewInit() {
+        return;
     }
 
     getList() {
@@ -44,6 +43,7 @@ export class CityPage implements OnInit, AfterViewInit {
                 this.dataSource = new MatTableDataSource<City>(
                     response.results,
                 );
+                console.log(response.results);
                 this.total = response.total;
             });
     }
@@ -55,7 +55,7 @@ export class CityPage implements OnInit, AfterViewInit {
                     title: 'Alert',
                     description: 'Are you sure to delete this City',
                     noText: 'Cancel',
-                    yesText: 'Delete'
+                    yesText: 'Delete',
                 },
             })
             .afterClosed()
@@ -76,84 +76,85 @@ export class CityPage implements OnInit, AfterViewInit {
             });
     }
 
-    editCity(id:string){
-        this.selectedId= id;
+    editCity(id: string) {
+        this.selectedId = id;
         const city = this.dataSource.data.find((x) => x.id == id);
         if (city) {
             this.name = city?.name;
             this.abbName = city?.abbName;
         }
-        const dialogRef = this.dialog.open(
-            FormDialogComponent,{
-                data: {
-                    id: this.selectedId,
-                    title: 'Edit City',
-                    noText: 'Cancel',
-                    yesText: 'Update',
-                    abbName: this.abbName,
-                    name: this.name
-                },
-            }
-        )
+        const dialogRef = this.dialog.open(FormDialogComponent, {
+            data: {
+                id: this.selectedId,
+                title: 'Edit City',
+                noText: 'Cancel',
+                yesText: 'Update',
+                abbName: this.abbName,
+                name: this.name,
+            },
+        });
 
-        dialogRef.afterClosed()
-        .subscribe((result)=>{
-            if(result){          
-                this.service.update({
-                    id: result.id,
-                    name: result.name,
-                    abbName: result.abbName,
-                }as City)
-                .subscribe((response)=>{
-                    this.getList();
-                    if(response){
-                        this.toast.success({
-                            detail: 'SUCCESS',
-                            summary: 'Update Successfully',
-                            duration: 3000,
-                        });
-                    }
-                })
-            }
-        })
-
-    }
-
-    addCity(){
-        this.resetForm();
-        const dialogRef = this.dialog.open(
-            FormDialogComponent,{
-                data: {
-                    title: 'Add City',
-                    noText: 'Cancel',
-                    yesText: 'Add',
-                    abbName: this.abbName,
-                    name: this.name
-                },
-            }
-        )
-            
-        dialogRef.afterClosed()
-        .subscribe((result) => {
-            if(result.name==""||result.abbName==""){
-                this.toast.error({detail:"Error Message", summary:"Add Fail, Try again later!", duration:5000})
-            }
-            else{
-                if (result) {
-                    this.service.add({name: result.name, abbName: result.abbName} as City ).subscribe((response) => {
+        dialogRef.afterClosed().subscribe((result) => {
+            if (result) {
+                this.service
+                    .update({
+                        id: result.id,
+                        name: result.name,
+                        abbName: result.abbName,
+                    } as City)
+                    .subscribe((response) => {
                         this.getList();
-    
                         if (response) {
                             this.toast.success({
                                 detail: 'SUCCESS',
-                                summary: 'Create Successfully',
+                                summary: 'Update Successfully',
                                 duration: 3000,
                             });
                         }
                     });
+            }
+        });
+    }
+
+    addCity() {
+        this.resetForm();
+        const dialogRef = this.dialog.open(FormDialogComponent, {
+            data: {
+                title: 'Add City',
+                noText: 'Cancel',
+                yesText: 'Add',
+                abbName: this.abbName,
+                name: this.name,
+            },
+        });
+
+        dialogRef.afterClosed().subscribe((result) => {
+            if (result.name == '' || result.abbName == '') {
+                this.toast.error({
+                    detail: 'Error Message',
+                    summary: 'Add Fail, Try again later!',
+                    duration: 5000,
+                });
+            } else {
+                if (result) {
+                    this.service
+                        .add({
+                            name: result.name,
+                            abbName: result.abbName,
+                        } as City)
+                        .subscribe((response) => {
+                            this.getList();
+
+                            if (response) {
+                                this.toast.success({
+                                    detail: 'SUCCESS',
+                                    summary: 'Create Successfully',
+                                    duration: 3000,
+                                });
+                            }
+                        });
                 }
             }
-            
         });
     }
 
@@ -167,5 +168,10 @@ export class CityPage implements OnInit, AfterViewInit {
         this.selectedId = '';
         this.name = '';
         this.abbName = '';
+    }
+
+    applyFilter(event: Event) {
+        const filterValue = (event.target as HTMLInputElement).value;
+        this.dataSource.filter = filterValue.trim().toLowerCase();
     }
 }
